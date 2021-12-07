a1241a2af58b13f03e00509af60e4caa19fe38b0
Benjamin Faber <bfaber@wisc.edu>
2021-12-07 08:57:41 -0600

    Initial commit of FieldlineTracing.jl, update project URLs to wistell

--- docs/make.jl
+++ docs/make.jl
@@ -5,12 +5,12 @@ DocMeta.setdocmeta!(FieldlineTracing, :DocTestSetup, :(using FieldlineTracing); 
 
 makedocs(;
     modules=[FieldlineTracing],
-    authors="Benjamin Faber <bfaber@wisc.edu> and contributors",
-    repo="https://gitlab.com/bfaber/FieldlineTracing.jl/blob/{commit}{path}#{line}",
+    authors="Benjamin Faber <bfaber@wisc.edu> and Aaron Bader <abader@engr.wisc.edu>",
+    repo="https://gitlab.com/wistell/FieldlineTracing.jl/blob/{commit}{path}#{line}",
     sitename="FieldlineTracing.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://bfaber.gitlab.io/FieldlineTracing.jl",
+        canonical="https://wistell.gitlab.io/FieldlineTracing.jl",
         assets=String[],
     ),
     pages=[
--- src/FieldlineTracing.jl
+++ src/FieldlineTracing.jl
@@ -1,5 +1,16 @@
 module FieldlineTracing
 
-# Write your package code here.
+using StaticArrays
+using NetCDF
+using Interpolations
+using ApproxFun
+using IntervalSets
+using DifferentialEquations
+using Polyester
+
+include("bfieldUtils.jl")
+include("bfieldTypes.jl")
+include("bfield.jl")
+include("followField.jl")
 
 end
