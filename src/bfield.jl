22cbe46a9c04529233b45a639b61b2ccb719d7b1
Benjamin Faber <bfaber@wisc.edu>
2022-06-01 09:55:08 -0500

    Cosmetic updates, changed CamelCase to snake_case

--- src/bfield.jl
+++ src/bfield.jl
@@ -13,76 +13,74 @@ Read a NetCDF BMW file and store the result in a `BField` type.
 
 # Arguemnts:
 - `filename::AbstractString`   # NetCDF BMW file name
-- `n_modes::Integer` = 0   # Number of modes used for Chebyshev interpolation
-- `padding::Integer` = 5   # Number of slices in ϕ padded to both ends of the magnetic grid arrays for accurate interpolation
 
 # See also: [`BField`](@ref)
 """
-function readBMW(filename::AbstractString;
-                )
-  bmwnetcdf = NetCDF.open(filename)
-  bmwVars = bmwnetcdf.vars
-  bmwDims = bmwnetcdf.dim
-#
-#
-  #there's probably a better way to do this, but damn the docs are awful
-  nr = 0
-  nz = 0
-  nphi = 0
-  for i in 1:length(bmwDims.vals)
-    #why the fuck there are undefined values here, who knows???
-    if isassigned(bmwDims.vals, i)
-      val = bmwDims.vals[i]
-      if val.name == "r"
-        nr = Int(val.dimlen)
-        @debug "Size of grid in r direction: $nr"
-      elseif val.name == "z"
-        nz = Int(val.dimlen)
-        @debug "Size of grid in z direction: $nz"
-      elseif val.name == "phi"
-        nphi = Int(val.dimlen)
-        @debug "Size of grid in ϕ direction: $nphi"
-      end
+function read_bmw(filename::AbstractString;
+                 )
+    bmwnetcdf = NetCDF.open(filename)
+    bmw_vars = bmwnetcdf.vars
+    bmw_dims = bmwnetcdf.dim
+    #
+    #
+    #there's probably a better way to do this, but damn the docs are awful
+    nr = 0
+    nz = 0
+    nphi = 0
+    for i in 1:length(bmw_dims.vals)
+        #why the fuck there are undefined values here, who knows???
+        if isassigned(bmw_dims.vals, i)
+            val = bmw_dims.vals[i]
+            if val.name == "r"
+                nr = Int(val.dimlen)
+                @debug "Size of grid in r direction: $nr"
+            elseif val.name == "z"
+                nz = Int(val.dimlen)
+                @debug "Size of grid in z direction: $nz"
+            elseif val.name == "phi"
+                nphi = Int(val.dimlen)
+                @debug "Size of grid in ϕ direction: $nphi"
+            end
+        end
     end
-  end
 
-  floatType = eltype(NetCDF.readvar(bmwVars["rmin"]))
+    float_type = eltype(NetCDF.readvar(bmw_vars["rmin"]))
 
-  bmw = BField(nr, nz, nphi; T=floatType)
+    bmw = BField(nr, nz, nphi; T=float_type)
 
-  #this loop looks through all the variables in bmwVars and
-  #checks if any appear in the array, if they do, then it
-  #copies the data into them
-  for var in bmwVars
-    varSymbol = Symbol(var.first)
-    if hasproperty(bmw, varSymbol)
-      T = typeof(getfield(bmw, varSymbol))
-      var_data = NetCDF.readvar(var.second)
-      if T <: Array
-        setfield!(bmw, varSymbol, convert(T,var_data))
-        @debug "Set BField variable $varSymbol"
-      elseif T <: Number
-        setfield!(bmw, varSymbol, convert(T,var_data[1]))
-        @debug "Set BField variable $varSymbol = $(getfield(bmw, varSymbol))"
-      elseif T <: String
-        setfield!(bmw, varSymbol, string(strip(string(var_data...))))
-        @debug "Set BField variable $varSymbol = $(getfield(bmw, varSymbol))"
-      else
-        throw(TypeError("No type given for $(var)!"))
-      end
+    #this loop looks through all the variables in bmw_vars and
+    #checks if any appear in the array, if they do, then it
+    #copies the data into them
+    for var in bmw_vars
+        var_symbol = Symbol(var.first)
+        if hasproperty(bmw, var_symbol)
+            T = typeof(getfield(bmw, var_symbol))
+            var_data = NetCDF.readvar(var.second)
+            if T <: Array
+                setfield!(bmw, var_symbol, convert(T,var_data))
+                @debug "Set BField variable $var_symbol"
+            elseif T <: Number
+                setfield!(bmw, var_symbol, convert(T,var_data[1]))
+                @debug "Set BField variable $var_symbol = $(getfield(bmw, var_symbol))"
+            elseif T <: String
+                setfield!(bmw, var_symbol, string(strip(string(var_data...))))
+                @debug "Set BField variable $var_symbol = $(getfield(bmw, var_symbol))"
+            else
+                throw(KeyError("No type given for $(var)!"))
+            end
+        end
     end
-  end
 
-  phimax = (2π)/bmw.nfp
-  bmw.r = LinRange(bmw.rmin, bmw.rmax, bmw.nr)
-  bmw.z = LinRange(bmw.zmin, bmw.zmax, bmw.nz)
-  bmw.phi = LinRange(0, phimax, bmw.nphi)
+    phi_max = (2π)/bmw.nfp
+    bmw.r = LinRange(bmw.rmin, bmw.rmax, bmw.nr)
+    bmw.z = LinRange(bmw.zmin, bmw.zmax, bmw.nz)
+    bmw.phi = LinRange(0, phi_max, bmw.nphi)
 
-  bmw.r_range = vector2range(bmw.r)
-  bmw.z_range = vector2range(bmw.z)
-  bmw.ϕ_range = vector2range(bmw.phi)
+    bmw.r_range = vector2range(bmw.r)
+    bmw.z_range = vector2range(bmw.z)
+    bmw.ϕ_range = vector2range(bmw.phi)
 
-  return bmw
+    return bmw
 end
 
 """
@@ -95,120 +93,121 @@ the coordinates `(x1,x2,x3)`.
 """
 function BFieldInterpolator(bfield::BField{T};
                            ) where {T}
-  ϕ_knots = bfield.ϕ_range
-  r_knots = bfield.r_range
-  z_knots = bfield.z_range
+    ϕ_knots = bfield.ϕ_range
+    r_knots = bfield.r_range
+    z_knots = bfield.z_range
 
-  Br = CubicSplineInterpolation((r_knots, z_knots, ϕ_knots), bfield.br_grid)
-  Bz = CubicSplineInterpolation((r_knots, z_knots, ϕ_knots), bfield.bz_grid)
-  Bϕ = CubicSplineInterpolation((r_knots, z_knots, ϕ_knots), bfield.bp_grid)
+    Br = CubicSplineInterpolation((r_knots, z_knots, ϕ_knots), bfield.br_grid)
+    Bz = CubicSplineInterpolation((r_knots, z_knots, ϕ_knots), bfield.bz_grid)
+    Bϕ = CubicSplineInterpolation((r_knots, z_knots, ϕ_knots), bfield.bp_grid)
 
-  BFieldInterpolator{T}(Br, Bz, Bϕ, bfield.nfp)
-
+    BFieldInterpolator{T}(Br, Bz, Bϕ, bfield.nfp)
 end
 
-function readMgrid(filename::AbstractString, current::Vector{Float64};
-                   n_modes::Integer = 0, padding::Integer=5,
+function readMgrid(filename::AbstractString,
+                   current::Vector{Float64};
+                   n_modes::Integer = 0,
+                   padding::Integer=5,
                   )
 
-  mgridnetcdf = NetCDF.open(filename)
-  mgridVars = mgridnetcdf.vars
-  mgridDims = mgridnetcdf.dim
+    mgridnetcdf = NetCDF.open(filename)
+    mgridVars = mgridnetcdf.vars
+    mgridDims = mgridnetcdf.dim
 
-  nr = 0
-  nz = 0
-  nphi = 0
-  external_coils = 1
-  for i in 1:length(mgridDims.vals)
-    if isassigned(mgridDims.vals, i)
-      val = mgridDims.vals[i]
-      if val.name == "rad"
-        nr = Int(val.dimlen)
-      elseif val.name == "zee"
-        nz = Int(val.dimlen)
-      elseif val.name == "phi"
-        nphi = Int(val.dimlen)
-      elseif val.name == "external_coils"
-        external_coils == Int(val.dimlen)
-      end
+    nr = 0
+    nz = 0
+    nphi = 0
+    external_coils = 1
+    for i in 1:length(mgridDims.vals)
+        if isassigned(mgridDims.vals, i)
+            val = mgridDims.vals[i]
+            if val.name == "rad"
+                nr = Int(val.dimlen)
+            elseif val.name == "zee"
+                nz = Int(val.dimlen)
+            elseif val.name == "phi"
+                nphi = Int(val.dimlen)
+            elseif val.name == "external_coils"
+                external_coils == Int(val.dimlen)
+            end
+        end
     end
-  end
-  n_modes = n_modes > div(nphi, 2) ? n_modes : div(nphi, 2)
-  floatType = eltype(NetCDF.readvar(mgridVars["rmin"]))
+    n_modes = n_modes > div(nphi, 2) ? n_modes : div(nphi, 2)
+    float_type = eltype(NetCDF.readvar(mgridVars["rmin"]))
 
-  mg = BField(nr, nz, nphi; n_modes=n_modes,
-               external_coils = external_coils, padding=padding)
+    mg = BField(nr, nz, nphi; n_modes=n_modes,
+                external_coils = external_coils, padding=padding)
 
-  mg.nr = nr
-  mg.nz = nz
-  mg.nphi = nphi
-  mg.n_modes = n_modes
-  mg.padding = padding
-  mg.external_coils = external_coils
+    mg.nr = nr
+    mg.nz = nz
+    mg.nphi = nphi
+    mg.n_modes = n_modes
+    mg.padding = padding
+    mg.external_coils = external_coils
 
-  bp_grid = zeros(floatType, nr, nz, nphi + 2*padding)
-  br_grid = zeros(floatType, nr, nz, nphi + 2*padding)
-  bz_grid = zeros(floatType, nr, nz, nphi + 2*padding)
+    bp_grid = zeros(float_type, nr, nz, nphi + 2*padding)
+    br_grid = zeros(float_type, nr, nz, nphi + 2*padding)
+    bz_grid = zeros(float_type, nr, nz, nphi + 2*padding)
 
-  b_temp = zeros(floatType, nr, nz, nphi + 2*padding)
+    b_temp = zeros(float_type, nr, nz, nphi + 2*padding)
 
-  for var in mgridVars
-    varSymbol = Symbol(var.first)
-    var_data = NetCDF.readvar(var.second)
-    varString = String(var.first)
+    for var in mgridVars
+        var_symbol = Symbol(var.first)
+        var_data = NetCDF.readvar(var.second)
+        varString = String(var.first)
 
-    #Special handling for the field values
-    if  (occursin("bp_", varString) || occursin("bz_", varString) ||
-        occursin("br_", varString))
+        #Special handling for the field values
+        if  (occursin("bp_", varString) || occursin("bz_", varString) ||
+            occursin("br_", varString))
 
 
-      T = typeof(getfield(mg, Symbol("bp_grid")))
-      #get the index
-      i = parse(Int64, varString[4:end])
-      ci = current[i]
-      fill!(b_temp, zero(eltype(T)))
+            T = typeof(getfield(mg, Symbol("bp_grid")))
+            #get the index
+            i = parse(Int64, varString[4:end])
+            ci = current[i]
+            fill!(b_temp, zero(eltype(T)))
 
-      b_temp[:,:,1+padding:nphi+padding] = convert.(eltype(T), var_data)
-      b_temp[:,:,1:padding] = b_temp[:, :, nphi+1:nphi+padding]
-      b_temp[:,:,nphi+padding+1:end]=b_temp[:,:,padding+1:2*padding]
-      b_temp *= ci
-      if occursin("bp_", varString)
-        bp_grid += copy(b_temp)
-      elseif occursin("bz_", varString)
-        bz_grid += copy(b_temp)
-      elseif occursin("br_", varString)
-        br_grid += copy(b_temp)
-      end
+            b_temp[:,:,1+padding:nphi+padding] = convert.(eltype(T), var_data)
+            b_temp[:,:,1:padding] = b_temp[:, :, nphi+1:nphi+padding]
+            b_temp[:,:,nphi+padding+1:end]=b_temp[:,:,padding+1:2*padding]
+            b_temp *= ci
+            if occursin("bp_", varString)
+                bp_grid += copy(b_temp)
+            elseif occursin("bz_", varString)
+                bz_grid += copy(b_temp)
+            elseif occursin("br_", varString)
+                br_grid += copy(b_temp)
+            end
 
-    #handle the other properties which are named the same
-    elseif hasproperty(mg, varSymbol)
-      T = typeof(getfield(mg, varSymbol))
-      if T <: Number
-        setfield!(mg, varSymbol, convert(T, var_data[1]))
-      elseif T <: String
-        setfield!(mg, varSymbol, string(strip(string(var_data...))))
-      else
-        throw(TypeError("No type given for $(var)!"))
-      end
+            #handle the other properties which are named the same
+            elseif hasproperty(mg, var_symbol)
+            T = typeof(getfield(mg, var_symbol))
+            if T <: Number
+                setfield!(mg, var_symbol, convert(T, var_data[1]))
+            elseif T <: String
+                setfield!(mg, var_symbol, string(strip(string(var_data...))))
+            else
+                throw(KeyError("No type given for $(var)!"))
+            end
+        end
+
     end
 
-  end
-
-  setfield!(mg, Symbol("bp_grid"), copy(bp_grid))
-  setfield!(mg, Symbol("br_grid"), copy(br_grid))
-  setfield!(mg, Symbol("bz_grid"), copy(bz_grid))
+    setfield!(mg, Symbol("bp_grid"), copy(bp_grid))
+    setfield!(mg, Symbol("br_grid"), copy(br_grid))
+    setfield!(mg, Symbol("bz_grid"), copy(bz_grid))
 
 
-  phimax = (2π)/mg.nfp
-  mg.r = LinRange(mg.rmin, mg.rmax, mg.nr)
-  mg.z = LinRange(mg.zmin, mg.zmax, mg.nz)
-  mg.phi = LinRange(0, phimax, mg.nphi)
+    phi_max = (2π)/mg.nfp
+    mg.r = LinRange(mg.rmin, mg.rmax, mg.nr)
+    mg.z = LinRange(mg.zmin, mg.zmax, mg.nz)
+    mg.phi = LinRange(0, phi_max, mg.nphi)
 
-  mg.r_range = vector2range(mg.r)
-  mg.z_range = vector2range(mg.z)
-  mg.ϕ_range = vector2range(mg.phi)
+    mg.r_range = vector2range(mg.r)
+    mg.z_range = vector2range(mg.z)
+    mg.ϕ_range = vector2range(mg.phi)
 
-  return mg
+    return mg
 
 end
 
--- src/followField.jl
+++ src/followField.jl
@@ -1,57 +1,60 @@
 struct InterpolationParameters{T}
-  itp::BFieldInterpolator{T}
-  values::Vector{T}
-  ϕ_max::T
+    itp::BFieldInterpolator{T}
+    values::Vector{T}
+    ϕ_max::T
 end
 
-function followField(itp::BFieldInterpolator{T},
-                     rzp::Array{Float64},
-                     phiEnd::Float64;
-                     phiStep::Float64=zero(T),
-                     poincare::Bool=false
-                    ) where {T}
-  phiStart = rzp[3]
-  u = @SVector [rzp[1], rzp[2]]
-  phiSpan = (phiStart,phiEnd)
-  params = InterpolationParameters(itp, zeros(T,3), 2π/itp.nfp)
-  prob = ODEProblem(fieldDerivPhi, u, phiSpan, params)
-  if poincare
-    N = abs(phiEnd - phiStart)/(2π/itp.nfp)
-    saveat = [i * 2*π/itp.nfp + phiStart for i in 1:N]
-  else
-    saveat = []
-  end
-  sol = abs(phiStep) > zero(T) ? solve(prob, Tsit5(), dtmax = phiStep, saveat = saveat) :
-                                 solve(prob, Tsit5(), saveat = saveat)
-
-end
-
-function followFieldS(itp::BFieldInterpolator{T},
+function follow_field(itp::BFieldInterpolator{T},
                       rzp::Array{Float64},
-                      sEnd::Float64;
-                      sStep::Float64=zero(T),
+                      phi_end::Float64;
+                      phi_step::Float64=zero(T),
+                      poincare::Bool=false
+                     ) where {T}
+    phiStart = rzp[3]
+    u = @SVector [rzp[1], rzp[2]]
+    phi_span = (phiStart,phi_end)
+    params = InterpolationParameters(itp, zeros(T,3), 2π/itp.nfp)
+    prob = ODEProblem(field_deriv_phi, u, phi_span, params)
+    if poincare
+        N = abs(phi_end - phiStart)/(2π/itp.nfp)
+        saveat = [i * 2*π/itp.nfp + phiStart for i in 1:N]
+    else
+        saveat = []
+    end
+    abs(phi_step) > zero(T) ? solve(prob, Tsit5(), dtmax = phi_step, saveat = saveat) :
+                              solve(prob, Tsit5(), saveat = saveat)
+
+end
+
+function follow_field_s(itp::BFieldInterpolator{T},
+                      rzp::Array{Float64},
+                      s_end::Float64;
+                      s_step::Float64=zero(T),
                       ) where{T}
-  u = @SVector [rzp[1], rzp[2], rzp[3]]
-  sSpan = (0, sEnd)
-  params = InterpolationParameters(itp, zeros(T,3), 2π/itp.nfp)
-  prob = ODEProblem(fieldDerivS, u, sSpan, params)
-  sol = abs(sStep) > zero(T) ? solve(prob, Tsit5(), dtmax = sStep, saveat = sStep) :
-                               solve(prob, Tsit5(), saveat = sStep)
+    u = @SVector [rzp[1], rzp[2], rzp[3]]
+    sSpan = (0, s_end)
+    params = InterpolationParameters(itp, zeros(T,3), 2π/itp.nfp)
+    prob = ODEProblem(field_deriv_s, u, sSpan, params)
+    abs(s_step) > zero(T) ? solve(prob, Tsit5(), dtmax = s_step, saveat = s_step) :
+                            solve(prob, Tsit5(), saveat = s_step)
 end
 
-function fieldDerivPhi!(du::Vector{Float64}, u::Vector{Float64},
-                        itp::BFieldInterpolator, ϕ::Float64)
-  r = u[1]
-  z = u[2]
-  br, bz, bp = itp(r, z, ϕ)
-  du[1] = r * br/bp
-  du[2] = r * bz/bp
+function field_deriv_phi!(du::Vector{Float64},
+                          u::Vector{Float64},
+                          itp::BFieldInterpolator,
+                          ϕ::Float64;
+                         )
+    r = u[1]
+    z = u[2]
+    br, bz, bp = itp(r, z, ϕ)
+    du[1] = r * br/bp
+    du[2] = r * bz/bp
 end
 
-function fieldDerivPhi(u::AbstractVector,
-                       itp::BFieldInterpolator{T},
-                       ϕ::Float64;
-                      ) where {T}
+function field_deriv_phi(u::AbstractVector,
+                         itp::BFieldInterpolator{T},
+                         ϕ::Float64;
+                        ) where {T}
   ϕ = mod(ϕ, 2π/itp.nfp)
   br, bz, bp = itp(u[1], u[2], ϕ)
   dr = u[1] * br/bp
@@ -59,29 +62,30 @@ function fieldDerivPhi(u::AbstractVector,
   SVector{2,T}(dr, dz)
 end
 
-
-function fieldDerivPhi(u::AbstractVector{T},
-                       p::InterpolationParameters{T},
-                       ϕ::T;
-                      ) where {T}
-  ϕ = mod(ϕ, p.ϕ_max)
-  map!(i->getfield(p.itp, i)(u[1], u[2], ϕ), p.values, 1:3)
-  bp = u[1] / p.values[3]
-  SVector{2,T}(bp * p.values[1], bp * p.values[2])
+"""
+"""
+function field_deriv_phi(u::AbstractVector{T},
+                         p::InterpolationParameters{T},
+                         ϕ::T;
+                        ) where {T}
+    ϕ = mod(ϕ, p.ϕ_max)
+    map!(i->getfield(p.itp, i)(u[1], u[2], ϕ), p.values, 1:3)
+    bp = u[1] / p.values[3]
+    SVector{2,T}(bp * p.values[1], bp * p.values[2])
 end
 
 
 #integration with respect to arclength
-function fieldDerivS(u::AbstractVector,
-                     p::InterpolationParameters{T},
-                     s::Float64;) where {T}
-  ϕ = mod(u[3], p.ϕ_max)
-  map!(i->getfield(p.itp, i)(u[1], u[2], ϕ), p.values, 1:3)
-  bmagsq = sum(p.values.^2)
-  dr = p.values[1]/bmagsq
-  dz = p.values[2]/bmagsq
-  dϕ = (p.values[3]/u[1])/bmagsq
-  SVector{3, T}(dr, dz, dϕ)
+function field_deriv_s(u::AbstractVector,
+                       p::InterpolationParameters{T},
+                       s::Float64;) where {T}
+    ϕ = mod(u[3], p.ϕ_max)
+    map!(i->getfield(p.itp, i)(u[1], u[2], ϕ), p.values, 1:3)
+    bmagsq = sum(p.values.^2)
+    dr = p.values[1]/bmagsq
+    dz = p.values[2]/bmagsq
+    dϕ = (p.values[3]/u[1])/bmagsq
+    SVector{3, T}(dr, dz, dϕ)
 
 end
 
@@ -99,8 +103,8 @@ function poincare(itp::BFieldInterpolator{T},
   params = InterpolationParameters(itp, zeros(T,3), ϕ_nfp)
   u0 = SVector{2,T}(first(initial_conditions)[1:2])
   ϕ_saveat = [i * ϕ_nfp + ϕ_start for i in 1:N]
-  prob = iszero(ϕ_step) ? ODEProblem(fieldDerivPhi, u0, ϕ_span, params, saveat = ϕ_saveat) :
-                          ODEProblem(fieldDerivPhi, u0, ϕ_span, params, saveat = ϕ_saveat, dtmax = ϕ_step)
+  prob = iszero(ϕ_step) ? ODEProblem(field_deriv_phi, u0, ϕ_span, params, saveat = ϕ_saveat) :
+                          ODEProblem(field_deriv_phi, u0, ϕ_span, params, saveat = ϕ_saveat, dtmax = ϕ_step)
 
   function prob_func(prob, i, repeat)
     @info "Remaking at iteration $i with initial condition $(initial_conditions[i][1:2])"
@@ -108,5 +112,5 @@ function poincare(itp::BFieldInterpolator{T},
   end
 
   poincare_prob = EnsembleProblem(prob, prob_func = prob_func)
-  traj = solve(poincare_prob, Tsit5(), EnsembleThreads(), trajectories = length(initial_conditions))
+  solve(poincare_prob, Tsit5(), EnsembleThreads(), trajectories = length(initial_conditions))
 end
