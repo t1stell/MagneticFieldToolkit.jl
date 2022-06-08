bbf50f82a49410e790d90818bb9380759023be62
aaroncbader <abader@engr.wisc.edu>
2022-06-08 13:20:28 -0500

    fix coordinates and grid errors in read bmw, read mgrid

--- src/followField.jl
+++ src/followField.jl
@@ -2,77 +2,97 @@ struct InterpolationParameters{T}
     itp::BFieldInterpolator{T}
     values::Vector{T}
     ϕ_max::T
+    r_min::T
+    r_max::T
+    z_min::T
+    z_max::T
 end
 
-function follow_field(itp::BFieldInterpolator{T},
-                      rzp::Array{Float64},
-                      phi_end::Float64;
-                      phi_step::Float64=zero(T),
+"""
+  Constructor for InterpolationParameters Struct
+"""
+function InterpolationParameters(itp::MagneticField{T}) where{T}
+  values = zeros(T, 3)
+  ϕ_max = maximum(itp.coords.θ)
+  r_max = maximum(itp.coords.r)
+  r_min = minimum(itp.coords.r)
+  z_max = maximum(itp.coords.z)
+  z_min = minimum(itp.coords.z)
+  return InterpolationParameters(itp, values, ϕ_max, r_min, r_max, z_min, z_max)
+end
+  
+
+function follow_field(itp::MagneticField{T},
+                      rϕz::Array{Float64},
+                      ϕ_end::Float64;
+                      ϕ_step::Float64=zero(T),
                       poincare::Bool=false
                      ) where {T}
-    phiStart = rzp[3]
-    u = @SVector [rzp[1], rzp[2]]
-    phi_span = (phiStart,phi_end)
-    params = InterpolationParameters(itp, zeros(T,3), 2π/itp.nfp)
-    prob = ODEProblem(field_deriv_phi, u, phi_span, params)
+    ϕ_start = rϕz[2]
+    u = @SVector [rϕz[1], rϕz[3]]
+    ϕ_span = (ϕ_start,ϕ_end)
+    params = InterpolationParameters(itp)
+    prob = ODEProblem(field_deriv_ϕ, u, ϕ_span, params)
     if poincare
-        N = abs(phi_end - phiStart)/(2π/itp.nfp)
-        saveat = [i * 2*π/itp.nfp + phiStart for i in 1:N]
+        N = abs(ϕ_end - ϕ_start)/(2π/itp.nfp)
+        saveat = [i * 2*π/itp.nfp + ϕ_start for i in 1:N]
     else
         saveat = []
     end
-    abs(phi_step) > zero(T) ? solve(prob, Tsit5(), dtmax = phi_step, saveat = saveat) :
+    abs(ϕ_step) > zero(T) ? solve(prob, Tsit5(), dtmax = ϕ_step, saveat = saveat) :
                               solve(prob, Tsit5(), saveat = saveat)
 
 end
 
-function follow_field_s(itp::BFieldInterpolator{T},
-                      rzp::Array{Float64},
+function follow_field_s(itp::MagneticField{T},
+                      rϕz::Array{Float64},
                       s_end::Float64;
                       s_step::Float64=zero(T),
                       ) where{T}
-    u = @SVector [rzp[1], rzp[2], rzp[3]]
-    sSpan = (0, s_end)
+    u = @SVector [rϕz[1], rϕz[2], rϕz[3]]
+    s_span = (0, s_end)
     params = InterpolationParameters(itp, zeros(T,3), 2π/itp.nfp)
-    prob = ODEProblem(field_deriv_s, u, sSpan, params)
+    prob = ODEProblem(field_deriv_s, u, s_span, params)
     abs(s_step) > zero(T) ? solve(prob, Tsit5(), dtmax = s_step, saveat = s_step) :
                             solve(prob, Tsit5(), saveat = s_step)
 end
 
-function field_deriv_phi!(du::Vector{Float64},
+function field_deriv_ϕ!(du::Vector{Float64},
                           u::Vector{Float64},
                           itp::BFieldInterpolator,
                           ϕ::Float64;
                          )
     r = u[1]
     z = u[2]
-    br, bz, bp = itp(r, z, ϕ)
-    du[1] = r * br/bp
-    du[2] = r * bz/bp
+    br, bϕ, bz = itp(r, ϕ, z)
+    du[1] = r * br/bϕ
+    du[2] = r * bz/bϕ
 end
 
-function field_deriv_phi(u::AbstractVector,
-                         itp::BFieldInterpolator{T},
+function field_deriv_ϕ(u::AbstractVector,
+                         itp::MagneticField{T},
                          ϕ::Float64;
                         ) where {T}
   ϕ = mod(ϕ, 2π/itp.nfp)
-  br, bz, bp = itp(u[1], u[2], ϕ)
-  dr = u[1] * br/bp
-  dz = u[1] * bz/bp
+  br, bϕ, bz = itp(u[1], ϕ, u[2])
+  dr = u[1] * br/bϕ
+  dz = u[1] * bz/bϕ
   SVector{2,T}(dr, dz)
 end
 
 """
 """
-function field_deriv_phi(u::AbstractVector{T},
+function field_deriv_ϕ(u::AbstractVector{T},
                          p::InterpolationParameters{T},
                          ϕ::T;
                         ) where {T}
     ϕ = mod(ϕ, p.ϕ_max)
-    if p.itp.r_min < u[1] < p.itp.r_max && p.itp.z_min < u[2] < p.itp.z_max
-        map!(i->getfield(p.itp, i)(u[1], u[2], ϕ), p.values, 1:3)
-        bp = u[1] / p.values[3]
-        return SVector{2,T}(bp * p.values[1], bp * p.values[2])
+    if p.r_min < u[1] < p.r_max && p.z_min < u[2] < p.z_max
+#        println("coords: ",ϕ," ",u)
+        p.values .= p.itp(u[1], ϕ, u[2])
+#        println("field:  ",p.values)
+        bϕ = u[1] / p.values[2]
+        return SVector{2,T}(bϕ * p.values[1], bϕ * p.values[3])
     else
         return u
     end
@@ -84,17 +104,17 @@ function field_deriv_s(u::AbstractVector,
                        p::InterpolationParameters{T},
                        s::Float64;) where {T}
     ϕ = mod(u[3], p.ϕ_max)
-    map!(i->getfield(p.itp, i)(u[1], u[2], ϕ), p.values, 1:3)
+    map!(i->getfield(p.itp, i)(u[1], ϕ, u[2]), p.values, 1:3)
     bmagsq = sum(p.values.^2)
     dr = p.values[1]/bmagsq
     dz = p.values[2]/bmagsq
     dϕ = (p.values[3]/u[1])/bmagsq
-    SVector{3, T}(dr, dz, dϕ)
+    SVector{3, T}(dr, dϕ, dz)
 
 end
 
-function poincare(itp::BFieldInterpolator{T},
-                  initial_conditions::AbstractArray;
+function poincare(itp::MagneticField,
+                  initial_conditions::AbstractArray{Vector{T}};
                   trace_ntransits::Integer = 1,
                   trace_nfp::Integer = 0,
                   ϕ_step::T=zero(T),
@@ -105,11 +125,11 @@ function poincare(itp::BFieldInterpolator{T},
   ϕ_start = last(first(initial_conditions))
   ϕ_end = N * ϕ_nfp + ϕ_start 
   ϕ_span = (ϕ_start, ϕ_end)
-  params = InterpolationParameters(itp, zeros(T,3), ϕ_nfp)
-  u0 = SVector{2,T}(first(initial_conditions)[1:2])
+  params = InterpolationParameters(itp)
+  u0 = SVector{2,T}(first(initial_conditions)[1],first(initial_conditions)[3])
   ϕ_saveat = [i * ϕ_nfp + ϕ_start for i in 1:N]
-  prob = iszero(ϕ_step) ? ODEProblem(field_deriv_phi, u0, ϕ_span, params, saveat = ϕ_saveat, maxiters = maxiters) :
-                          ODEProblem(field_deriv_phi, u0, ϕ_span, params, saveat = ϕ_saveat, maxiters = maxiters, dtmax = ϕ_step)
+  prob = iszero(ϕ_step) ? ODEProblem(field_deriv_ϕ, u0, ϕ_span, params, saveat = ϕ_saveat, maxiters = maxiters) :
+                          ODEProblem(field_deriv_ϕ, u0, ϕ_span, params, saveat = ϕ_saveat, maxiters = maxiters, dtmax = ϕ_step)
 
   function prob_func(prob, i, repeat)
     @debug "Remaking trajectory $i with initial condition $(initial_conditions[i][1:2])"
@@ -139,7 +159,7 @@ function poincare(bfield::BField,
     z_grid = reshape(repeat(z₀, outer= length(r₀)), full_size)
     init_cond = Matrix{Vector{T}}(undef, full_size)
     for i in eachindex(r_grid, z_grid, init_cond)
-        init_cond[i] = [r_grid[i], z_grid[i], ϕ₀]
+        init_cond[i] = [r_grid[i], ϕ₀, z_grid[i]]
     end
 
     poincare(itp, init_cond;
