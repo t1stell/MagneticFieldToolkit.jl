04026536bd07f3b982cfcbb2b18f9793c38c7737
aaroncbader <abader@engr.wisc.edu>
2022-11-25 10:31:19 -0600

    allow for following field from coils.  Can be improved and refactored still

--- src/MagneticFieldToolkit.jl
+++ src/MagneticFieldToolkit.jl
@@ -15,15 +15,21 @@ using PlasmaEquilibriumToolkit
 # File load 
 export read_mgrid, read_bmw, read_vmec_coils, read_mgrid_h5
 
+#Structs and constructors
+export CoilSet
+
 # Field calculations
 export compute_magnetic_potential, compute_magnetic_field
 export generate_mgrid
 
+# Following calculations
+export follow_field, follow_field_s
+
 include("bfieldUtils.jl")
 include("ReadMagneticField.jl")
-include("followField.jl")
 include("QuadraticFluxMinimize.jl")
 include("Coils.jl")
+include("followField.jl")
 
 function __init__()
     @require CairoMakie="13f3f980-e62b-5c42-98c6-ff1f3baf88f0" include("plot_makie.jl")
